5cbfa3402b1e5307aacba81b042ab25879f38476
ssddi456 <torrent88410@gmail.com>
2021-11-26 16:07:47 +0800

    update

--- test/index.tsx
+++ test/index.tsx
@@ -1,5 +1,7 @@
 import styles from './index.module.css';
 import testStyles from './test.css';
+import testLessStyle from './test.less';
 
 console.log(styles.error);
 console.log(testStyles.error);
+console.log(testLessStyle.other_less_config);
